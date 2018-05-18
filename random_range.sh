99066998fbaca1923d6d91ac0d9009850d884b23
kroneyen <krone.huang@gmail.com>
2018-05-18 17:51:58 +0800

    adding no-ip

--- random_range.sh
+++ random_range.sh
@@ -32,18 +32,27 @@ function random_range()
 ## delete crontab
 sed -i '/p2plogin_linux.py/d' /var/spool/cron/$USER
 sed -i '/kingbus_linux.py/d' /var/spool/cron/$USER
+sed -i '/no_ip_confirm.py/d' /var/spool/cron/$USER
 
 ## add crontab
 
-##call  function h_start h_end m_start m_end
+##call  function of p2p h_start h_end m_start m_end
 random_range 1 6 1 59
 echo "$_m $_h  * * * cd /root/python_dir/p2plogin && /usr/local/bin/python3.6 p2plogin_linux.py" >> "/var/spool/cron/$USER"
 
 sleep 0.5
 
-##call  function h_start h_end m_start m_end
+##call  function of kingbus h_start h_end m_start m_end
 random_range 1 6 1 59
 echo "$_m $_h  * * 1 cd /root/python_dir/kingbus && /usr/local/bin/python3.6 kingbus_linux.py" >> "/var/spool/cron/$USER"
+
+sleep 0.5
+
+##call  function of no-ip h_start h_end m_start m_end
+random_range 1 6 1 59
+echo "$_m $_h  * * 1 cd /root/python_dir/no_ip && /usr/local/bin/python3.6 no_ip_confirm.py" >> "/var/spool/cron/$USER"
+
+
 
 ## crontab apply
 /usr/bin/crontab /var/spool/cron/$USER
