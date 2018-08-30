b15804cda59a31a0b9047b4e5a4fecae59340eff
kroneyen <krone.huang@gmail.com>
2018-08-30 12:18:33 +0800

    adding  flush_log function
    adding  flush_log function

--- random_range.sh
+++ random_range.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#20180330 adding flush_logs clean more than 10 M
+
 
 function random_range()
 {
@@ -28,19 +30,44 @@ function random_range()
 #    echo $_h $_m	
 }
 
+function flush_logs()
+{
+
+##flush_logs $1(path) $2(log_file_locate)
+
+cd $1
+
+file=`cat $2 | grep filename | awk '{print $1}' | cut -c 11- |cut -d "'" -f 1` ## get log_file name
+
+file_size=`du -m ${file} | awk '{print $1}'`
+    
+#echo $file_size
+
+if [ "$file_size" -ge "10" ]; then   ## log_file  >= 10M
+   echo "" > $file
+
+fi
+
+}
+
+
 
 ## delete crontab
-sed -i '/p2plogin_linux.py/d' /var/spool/cron/$USER
+sed -i '/p2plogin_linux_with_reply.py/d' /var/spool/cron/$USER
 sed -i '/kingbus_linux.py/d' /var/spool/cron/$USER
-sed -i '/no_ip_confirm.py/d' /var/spool/cron/$USER
+#sed -i '/no_ip_confirm.py/d' /var/spool/cron/$USER
+sed -i '/apk_linux_with_reply.py/d' /var/spool/cron/$USER
 
 ## add crontab
 
 ##call  function of p2p h_start h_end m_start m_end
 random_range 1 6 1 59
-echo "$_m $_h  * * * cd /root/python_dir/p2plogin && /usr/local/bin/python3.6 p2plogin_linux.py" >> "/var/spool/cron/$USER"
+echo "$_m $_h  * * * cd /root/python_dir/p2plogin && /usr/local/bin/python3.6 p2plogin_linux_with_reply.py" >> "/var/spool/cron/$USER"
 
 sleep 0.5
+##call function flush_logs(path log_file_locate)
+flush_logs '/root/python_dir/p2plogin' 'p2plogin_linux_with_reply.py'
+
 
 ##call  function of kingbus h_start h_end m_start m_end
 random_range 1 6 1 59
@@ -48,11 +75,23 @@ echo "$_m $_h  * * 1 cd /root/python_dir/kingbus && /usr/local/bin/python3.6 kin
 
 sleep 0.5
 
+##call function flush_logs(path log_file_locate)
+flush_logs '/root/python_dir/kingbus' 'kingbus_linux.py'
+
 ##call  function of no-ip h_start h_end m_start m_end
+#random_range 1 6 1 59
+#echo "$_m $_h  * * 1 cd /root/python_dir/no_ip && /usr/local/bin/python3.6 no_ip_confirm.py" >> "/var/spool/cron/$USER"
+
+
+
+##call  function of apk_linux_with_reply h_start h_end m_start m_end
 random_range 1 6 1 59
-echo "$_m $_h  * * 1 cd /root/python_dir/no_ip && /usr/local/bin/python3.6 no_ip_confirm.py" >> "/var/spool/cron/$USER"
+echo "$_m $_h  * * * cd /root/python_dir/apklogin && /usr/local/bin/python3.6 apk_linux_with_reply.py" >> "/var/spool/cron/$USER"
 
+sleep 0.5
 
+##call function flush_logs(path log_file_locate)
+flush_logs '/root/python_dir/apklogin' 'apk_linux_with_reply.py'
 
 ## crontab apply
 /usr/bin/crontab /var/spool/cron/$USER
