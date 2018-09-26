a09f814dd8b970485035a06a4869d6f19f7e17c7
kroneyen <krone.huang@gmail.com>
2018-09-26 14:52:22 +0800

    use array index

--- py_log_check.sh
+++ py_log_check.sh
@@ -3,29 +3,26 @@
 _file='/root/python_dir'
 _list=('p2plogin' 'apklogin' 'kingbus')
 
-for list in "${_list[@]}";do
+for index in "${!_list[@]}";do  ## length:${#_file[@]} , index:${!_file[@]}
 
-  _path=${_file}/${list}
+  _path=${_file}/${_list[index]}
   cd ${_path} 
-  if [ ${list} == 'p2plogin' ];then
+  if [ $index -eq 0 ];then
      _strr="p2p*.log"     
   else 
      _strr="*.log"
   fi
-  
-  #IFS=', ' read -r -a _logs <<< `ls -al $_strr | awk '{print $9}'`
   read -r -a _logs <<< `ls -al $_strr | awk '{print $9}'`  ##into array
 
   for log in "${_logs[@]}" 
   do
      echo $log
      echo "======= start ======="
-     tail -n45 ${log} | while read line || [[ -n "$line" ]]; do
+     tail -n45 ${log} | while read line ; do
       echo $line
      done
      #sleep 1
      echo "======= done  ======"
      echo "                    "
   done
-
 done
