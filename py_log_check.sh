6d615bb01495298f53f293a225f43b01ab3aa1c3
kroneyen <krone.huang@gmail.com>
2018-12-20 10:57:03 +0800

    add except condition for log file

--- py_log_check.sh
+++ py_log_check.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 _file='/root/python_dir'
-_list=('p2plogin' 'apklogin' 'kingbus')
+_list=('p2plogin' 'apklogin' 'awabest' 'kingbus')
 
 for index in "${!_list[@]}";do  ## length:${#_file[@]} , index:${!_file[@]}
 
   _path=${_file}/${_list[index]}
+  _strr='*.log' 
+  _v_strr='history\|test' ## except word
+  
   cd ${_path} 
-  if [ $index -eq 0 ];then
-     _strr="p2p*.log"     
-  else 
-     _strr="*.log"
-  fi
-  read -r -a _logs <<< `ls -al $_strr | awk '{print $9}'`  ##into array
+  read -r -a _logs <<< `ls -al ${_strr} | awk '{print $9}' | grep -v ${_v_strr}`  ## find log name into array
 
   for log in "${_logs[@]}" 
   do
