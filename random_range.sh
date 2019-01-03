71dcce269def82bc05fe2fdfe2c4c544481366cc
kroneyen <krone.huang@gmail.com>
2019-01-03 10:29:35 +0800

    add 2 parameter of weekly

--- random_range.sh
+++ random_range.sh
@@ -4,7 +4,7 @@
 
 function random_range()
 {
-    if [ "$#" -lt "6" ]; then 
+    if [ "$#" -lt "8" ]; then 
         echo "Usage: random_range <low> <high>"
         return
     fi
@@ -14,6 +14,8 @@ function random_range()
     range_1=$(($4 -$3 +1)) ##1~59
     low_2=$5
     range_2=$(($6 -$5 +1)) ##1~7
+    low_3=$7
+    range_3=$(($8 -$7 +1)) ##1~7
 
     _h=$(($low+$RANDOM % $range)) ## for hours
     	
@@ -28,12 +30,23 @@ function random_range()
            _m=$(printf "%02d" $_m)
         fi
 
-    _w=$(($low_2+$RANDOM % $range_2))  ## for week
+    _w=1
+    _w1=1
 
-        if [ "$_w" -lt "10" ]; then
-           _w=$(printf "%01d" $_w)
-        fi
+    while [ "$_w" -eq "$_w1" ]
+    do
+       _w=$(($low_2+$RANDOM % $range_2))  ## for week
 
+          if [ "$_w" -lt "10" ]; then
+             _w=$(printf "%01d" $_w)
+          fi
+
+       _w1=$(($low_3+$RANDOM % $range_3))  ## for week
+
+          if [ "$_w1" -lt "10" ]; then
+             _w1=$(printf "%01d" $_w1)
+          fi
+    done
    
 #    echo $_h $_m	
 }
@@ -59,7 +72,7 @@ fi
 
 }
 
-
+_random_list="1 5 1 59 1 7 1 7"
 
 ## delete crontab
 sed -i '/p2plogin_linux_with_reply.py/d' /var/spool/cron/$USER
@@ -73,7 +86,7 @@ sed -i '/awa_linux_with_reply.py/d' /var/spool/cron/$USER
 ## add crontab
 
 ##call  function of p2p h_start h_end m_start m_end w_start w_end
-random_range 1 5 1 59 1 7
+random_range $_random_list 
 echo "$_m $_h  * * * cd /root/python_dir/p2plogin && /usr/local/bin/python3.6 p2plogin_linux_with_reply.py" >> "/var/spool/cron/$USER"
 
 sleep 0.5
@@ -82,7 +95,7 @@ flush_logs '/root/python_dir/p2plogin' 'p2plogin_linux_with_reply.py'
 
 
 ##call  function of kingbus h_start h_end m_start m_end w_start w_end
-random_range 1 5 1 59 1 7
+random_range $_random_list
 echo "$_m $_h  * * 1 cd /root/python_dir/kingbus && /usr/local/bin/python3.6 kingbus_linux.py" >> "/var/spool/cron/$USER"
 
 sleep 0.5
@@ -91,7 +104,7 @@ sleep 0.5
 flush_logs '/root/python_dir/kingbus' 'kingbus_linux.py'
 
 ##call  function of no-ip h_start h_end m_start m_end  by month-day per-month/12
-random_range 1 5 1 59 1 7
+random_range $_random_list
 echo "$_m $_h  9 * * cd /root/python_dir/no_ip && /usr/local/bin/python3.6 no_ip_confirm.py" >> "/var/spool/cron/$USER"
 
 sleep 0.5
@@ -100,7 +113,7 @@ flush_logs '/root/python_dir/no_ip' 'no_ip_confirm.py'
 
 
 ##call  function of apk_linux_with_reply h_start h_end m_start m_end w_start w_end
-random_range 1 5 1 59 1 7
+random_range $_random_list
 echo "$_m $_h  * * * cd /root/python_dir/apklogin && /usr/local/bin/python3.6 apk_linux_with_reply.py" >> "/var/spool/cron/$USER"
 
 sleep 0.5
@@ -110,8 +123,8 @@ flush_logs '/root/python_dir/apklogin' 'apk_linux_with_reply.py'
 
 
 ## call  function of drama h_start h_end m_start m_end w_start w_end
-random_range 1 5 1 59 1 7
-echo "$_m $_h  * * $_w cd /root/python_dir/p2plogin && /usr/local/bin/python3.6 p2plogin_linux_with_reply_drama.py" >> "/var/spool/cron/$USER"
+random_range $_random_list
+echo "$_m $_h  * * $_w,$_w1 cd /root/python_dir/p2plogin && /usr/local/bin/python3.6 p2plogin_linux_with_reply_drama.py" >> "/var/spool/cron/$USER"
 
 sleep 0.5
 ##call function flush_logs(path log_file_locate)
@@ -119,15 +132,15 @@ flush_logs '/root/python_dir/p2plogin' 'p2plogin_linux_with_reply_drama.py'
 
 
 ## call  function of software h_start h_end m_start m_end w_start w_end
-random_range 1 5 1 59 1 7
-echo "$_m $_h  * * $_w cd /root/python_dir/p2plogin && /usr/local/bin/python3.6 p2plogin_linux_with_reply_software.py" >> "/var/spool/cron/$USER"
+random_range $_random_list
+echo "$_m $_h  * * $_w,$_w1 cd /root/python_dir/p2plogin && /usr/local/bin/python3.6 p2plogin_linux_with_reply_software.py" >> "/var/spool/cron/$USER"
 
 sleep 0.5
 ##call function flush_logs(path log_file_locate)
 flush_logs '/root/python_dir/p2plogin' 'p2plogin_linux_with_reply_software.py'
 
 ## call  function of awabest  h_start h_end m_start m_end w_start w_end
-random_range 1 5 1 59 1 7
+random_range $_random_list
 echo "$_m $_h  * * * cd /root/python_dir/awabest && /usr/local/bin/python3.6 awa_linux_with_reply.py" >> "/var/spool/cron/$USER"
 
 sleep 0.5
