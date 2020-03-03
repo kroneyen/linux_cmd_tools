5f938b007a3f0acb38c7906bd35d230506a35d46
kroneyen <krone.huang@gmail.com>
2020-03-03 16:02:50 +0800

    Update Deadlock_Check.sh
    fix bugger :  innodb status content incorrect

--- Deadlock_Check.sh
+++ Deadlock_Check.sh
@@ -4,30 +4,32 @@ Today=`date +"%Y-%m-%d %H:%M"`
 D_log='innodb_status.log'
 D_temp='Deadlock_temp.txt'
 
-Deadlock_Qry=`mysql -pflush_user -uflush_user -hlocalhost -e 'show engine innodb status\G' | grep -A 1000 "LATEST DETECTED DEADLOCK"  | grep -B 1000 "WE ROLL BACK TRANSACTION"`
-Deadlock_id=`echo $Deadlock_Qry  | awk '{print $7}'`
-mail_group="krone.huang@gmail"
+Deadlock_Qry=`mysql -pflush_user -uflush_user -hlocalhost -e 'show engine innodb status\G' | grep -A 1000 "LATEST DETECTED DEADLOCK"  | grep -B 1000 "WE ROLL BACK TRANSACTION" > ${D_temp}`
+Deadlock_id=`cat $D_temp | grep -A 2 'LATEST DETECTED DEADLOCK' | awk '{print $3}' | tail -n 1`
+mail_group='krone.yen@ingeniousinc.co lenny@ingeniousinc.co eric.chung@ingeniousinc.co'
 
-echo $Deadlock_Qry > $D_temp
 
-if [ -e $D_log ] ; then
-
-   log_Deadlock_id=`cat $D_log | grep -A 2 'LATEST DETECTED DEADLOCK' | tail -n 1 | awk '{print $7}'`
+if [ -e $D_log ] ; then 
+       
+   log_Deadlock_id=`cat $D_log | grep -A 2 'LATEST DETECTED DEADLOCK' | awk '{print $3}' | tail -n 1`
 
    if [ "$Deadlock_id" != "$log_Deadlock_id" ] ;then
 
-     mail -s "Mysql Deadlock Issue $Today" -S "smtp-use-starttls=yes" -S "sntp-auth=login" -S "smtp=smtp.gmail.com:587"  -S "smtp-auth-user=ingeniousinc2016@gmail.com" -S "smtp-auth-password=Qwer2016" -S "from=ingeniousinc2016@gmail.com" -S "ssl-verify=ignore" -S "nss-config-dir=/etc/pki/nssdb/.certs" $mail_group  < $D_temp
-     echo $Deadlock_Qry > $D_log
+     mail -s "Mysql Deadlock Issue $Today" -S "smtp-use-starttls=yes" -S "sntp-auth=login" -S "smtp=smtp.gmail.com:587"  -S "smtp-auth-user=rd@1-pay.co" -S "smtp-auth-password=mg66=FBh" -S "from=rd@1-pay.co" -S "ssl-verify=ignore" -S "nss-config-dir=/root/DB_data/.certs" $mail_group  < $D_temp
+     cat $D_temp > $D_log
    else
       echo "Innodb Without Deadlock"
    fi
 
 
-else
-
-
-     mail -s "Mysql Deadlock Issue $Today" -S "smtp-use-starttls=yes" -S "sntp-auth=login" -S "smtp=smtp.gmail.com:587"  -S "smtp-auth-user=ingeniousinc2016@gmail.com" -S "smtp-auth-password=Qwer2016" -S "from=ingeniousinc2016@gmail.com" -S "ssl-verify=ignore" -S "nss-config-dir=/etc/pki/nssdb/.certs" $mail_group  < $D_temp
-
-      echo $Deadlock_Qry > $D_log
+else 
+   
+   check_temp_len=`wc ${D_temp}  | awk '{print $2}'`         
+   if [ $check_temp_len -gt 1  ] ; then
+        mail -s "Mysql Deadlock Issue $Today" -S "smtp-use-starttls=yes" -S "sntp-auth=login" -S "smtp=smtp.gmail.com:587"  -S "smtp-auth-user=rd@1-pay.co" -S "smtp-auth-password=mg66=FBh" -S "from=rd@1-pay.co" -S "ssl-verify=ignore" -S "nss-config-dir=/root/DB_data/.certs" $mail_group  < $D_temp
+     
+       cat $D_temp > $D_log
+   fi
+  
 
 fi
