3d49a26f03acbd5ed6e5b563d4b66a0ef559edce
kroneyen <krone.huang@gmail.com>
2020-08-12 15:38:45 +0800

    Dynamic parameter for db name

--- auto_optimze_table.sh
+++ auto_optimze_table.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
 ##install percona toolkit 
+#!/bin/sh
 
-alter_files='/root/alter_table_list'
-alter_log='/root/alter_info.log'
+##install percona toolkit 
+##sh auto_optimze_table.sh  payment_pool_uft8mb4
+_user='root'
+_pwd='PWD'
+_db=$1
+alter_files='alter_table_list'
+alter_log='alter_info.log'
 
-process_num=`ps -ef | grep "auto_optimze_table.sh" | grep -v "grep" | wc -l`
+process_num=`ps -ef | grep $0 | grep -v 'grep' | wc -l`
+
 
 if [ $process_num -eq 2 ] ; then
 
-mysql -p123qweasd -uroot -e "SELECT CONCAT('pt-online-schema-change --user=root --password=123qweasd --alter ENGINE=InnoDB D=',table_schema,',t=',table_name,' --execute') as t FROM information_schema.tables  WHERE engine='InnoDB'  AND table_schema NOT REGEXP('information_schema|mysql|performance_schema');" |  grep 'pt-online-schema-change'  > ${alter_files}
+/usr/bin/mysql -p${_pwd} -u${_user} -e "SELECT CONCAT('/usr/bin/pt-online-schema-change --user=${_user} --password=${_pwd} --alter ENGINE=InnoDB D=',table_schema,',t=',table_name,' --execute') as t FROM information_schema.tables  WHERE engine='InnoDB'  AND table_schema NOT REGEXP('information_schema|mysql|performance_schema') order by table_name;" |  grep 'pt-online-schema-change' | grep ${_db} > ${alter_files}
+
 
 echo "" > ${alter_log}
 
@@ -17,19 +25,16 @@ while read line
 do
 
 
-	$line >> ${alter_log}
+        $line >> ${alter_log}
 
 
 done  < ${alter_files}
 
 
-else 
+else
 
-	echo "process is runing!!!"
+        echo "process is runing!!!"
 
 
 fi
-
-
-
 
